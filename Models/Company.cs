8d1096aa426855e3c8c39cea5f463aae85693bbd
dan.pitu <dan.pitu@gmail.com>
2019-02-10 20:50:21 -0500

    init

---
+++ Models/Company.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace JQueryDataTables.Models
+{
+    /// <summary>
+    /// Company class contains information that shoudl be shown in table
+    /// </summary>
+    public class Company
+    {
+        static int nextID = 17;
+
+        public Company()
+        {
+            ID = nextID++;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+    }
+
+}
